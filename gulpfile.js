4fb81c1f615867e0b69f26d7961dc48fa2551475
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-10-19 18:44:25 +0200

    Remove image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,14 @@
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 
-gulp.task('optimize-images', async _ => {
-    const imagemin = (await import('gulp-imagemin')).default;
-    const mozjpeg = (await import('imagemin-mozjpeg')).default;
-    const optipng = (await import('imagemin-optipng')).default;
-    const svgo = (await import('imagemin-svgo')).default;
-
-    return gulp.src('./src/assets/**/*.{png,jpg,jpeg,gif,svg}')
-        .pipe(imagemin([
-            mozjpeg({ quality: 75, progressive: true }),
-            optipng({ optimizationLevel: 5 }),
-            svgo({
-                plugins: [
-                    { removeViewBox: false },
-                    { cleanupIDs: false }
-                ]
-            })
-        ]))
-        .pipe(gulp.dest('src/assets'));
-});
-
 gulp.task('build-date', _ => {
     const now = new Date();
     const buildDate = now.toISOString();
-    
-    return gulp.src('./src/environments/environment.prod.ts') 
-        .pipe(replace(/buildDate: '.*'/, `buildDate: '${buildDate}'`)) 
-        .pipe(gulp.dest('./src/environments/')) 
+
+    return gulp.src('./src/environments/environment.prod.ts')
+        .pipe(replace(/buildDate: '.*'/, `buildDate: '${buildDate}'`))
+        .pipe(gulp.dest('./src/environments/'))
         .on('end', () => console.log(`⚠️ Build date set to ${buildDate}`));
 });
 
-gulp.task('post:install', gulp.series('optimize-images'));
 gulp.task('build:date', gulp.series('build-date'));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   cloudFns: false,
   mail: 'pmmiklas@gmail.com',
-  buildDate: '2024-10-19T16:30:37.234Z'
+  buildDate: '2024-10-19T17:08:35.612Z'
 };
