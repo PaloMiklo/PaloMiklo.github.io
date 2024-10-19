7b1fe3f646e08474846603c7e2c11278ca9ec6ba
PaloMiklo <31817682+PaloMiklo@users.noreply.github.com>
2024-10-19 18:30:36 +0200

    Optimize production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   cloudFns: false,
   mail: 'pmmiklas@gmail.com',
-  buildDate: '2024-10-19T16:23:32.243Z'
+  buildDate: '2024-10-19T16:30:37.234Z'
 };
